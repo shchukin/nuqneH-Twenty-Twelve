7f139cf967e611b980e3f3b3b92b543b76f8eb70
Cypher-kun <cypher76@gmail.com>
2013-02-05 00:10:05 +0400

    Featured image and page title were swapped

--- content.php
+++ content.php
@@ -15,14 +15,16 @@
 		</div>
 		<?php endif; ?>
 		<header class="entry-header">
-			<?php the_post_thumbnail(); ?>
+			
 			<?php if ( is_single() ) : ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<?php the_post_thumbnail(); ?>
 			
 			<?php else : ?>
 			<h1 class="entry-title">
 				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h1>
+			<?php the_post_thumbnail(); ?>
 			
 			<?php endif; // is_single() ?>
 			<?php if ( comments_open() ) : ?>
