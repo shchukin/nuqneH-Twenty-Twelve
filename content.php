3dd375f13eea5d2327f41ddf231b8b764fac6181
Cypher-kun <cypher76@gmail.com>
2013-02-28 03:05:19 +0400

    Featured image was added to page template

--- content.php
+++ content.php
@@ -17,15 +17,13 @@
 		<header class="entry-header">
 			
 			<?php if ( is_single() ) : ?>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-			<?php the_post_thumbnail(); ?>
-			
+			     <h1 class="entry-title"><?php the_title(); ?></h1>
+			     <?php the_post_thumbnail(); ?>
 			<?php else : ?>
-			<h1 class="entry-title">
-				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-			</h1>
-			<?php the_post_thumbnail(); ?>
-			
+    			<h1 class="entry-title">
+    				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+    			</h1>
+    			<?php the_post_thumbnail(); ?>
 			<?php endif; // is_single() ?>
 
 		</header><!-- .entry-header -->
